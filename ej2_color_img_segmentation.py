1411ebd4a0f60583ac50623c2113e99fca20c22b
Paula Oseroff <oseroffp@gmail.com>
2023-10-01 20:58:25 -0300

    feat(ej2): add svm

--- ej2_color_img_segmentation.py
+++ ej2_color_img_segmentation.py
@@ -1,16 +1,24 @@
 import pandas as pd
-from sklearn import svm
+from sklearn.svm import SVC
 from utils.img_utils import img_to_classified_df, img_to_df
 from utils.data_split import k_fold_split
+from utils.sklearn_utils import c_to_sklearn_c
 
 def run_ej2():
     cow_df = img_to_classified_df("data/vaca.jpg", 'cow')
     grass_df = img_to_classified_df("data/pasto.jpg", 'grass')
     sky_df = img_to_classified_df("data/cielo.jpg", 'sky')
     df = pd.concat([cow_df, grass_df, sky_df], ignore_index=True)
-    # print(df)
     sample_df = img_to_df("data/cow.jpg")
     train_df, test_df = k_fold_split(df, k=5)
+
+    X = train_df[["R", "G", "B"]].to_numpy()
+    Y = train_df["Class"].to_numpy()
+
+    sk_c = c_to_sklearn_c(1)
+
+    svm = SVC(C=sk_c, kernel="rbf")
+    svm.fit(X, Y)
 
 if __name__ == '__main__':
     run_ej2()
---
+++ utils/sklearn_utils.py
@@ -0,0 +1,2 @@
+def c_to_sklearn_c(c : float) -> float:
+    return 1/c

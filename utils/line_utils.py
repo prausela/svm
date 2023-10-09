4a9f1f76fd891e68efe4736d50afc4d5e6701923
Paula Oseroff <oseroffp@gmail.com>
2023-10-09 02:12:43 -0300

    feat(line_utils): add line class and compute distance

--- utils/line_utils.py
+++ utils/line_utils.py
@@ -1,18 +1,27 @@
-from utils.perceptron import Perceptron
+from utils.perceptron import Perceptron, __add_bias__
+from utils.matrix_utils import single_column_array_shape, single_row_array_shape, mat_norm_2
 
 import pandas as pd
 import numpy as np
 
 class Line:
 
-    def __init__(self, w: np.ndarray) -> None:
-        pass
+    def __init__(self, coeffs: np.ndarray) -> None:
+        coeffs = single_column_array_shape(coeffs, 3)
 
-    def evaluate(self, point: np.ndarray) -> float:
-        pass
+        no_bias_coeffs = coeffs[:-1, :]
+        self.coeffs = coeffs / mat_norm_2(no_bias_coeffs)
+
+    def distance_to(self, point: np.ndarray) -> float:
+        point = single_row_array_shape(point, 2)
+        point = __add_bias__(point)
+
+        single_val_mat = np.dot(point, self.coeffs)
+
+        return single_val_mat.item()
 
     def coefficients(self) -> np.ndarray:
-        pass
+        return np.copy(self.coeffs)
 
 def step_perceptron_line(perceptron: Perceptron) -> Line:
     pass

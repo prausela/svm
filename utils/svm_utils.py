9bbe5713a063b99bf6f90136655f8ea8a1d9810c
Luciana Diaz Kralj <lucidiaz@itba.edu.ar>
2023-10-09 00:06:09 -0300

    feat(svm_utils): added cost and gradient functions

--- utils/svm_utils.py
+++ utils/svm_utils.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
 from sklearn.svm import SVC
+from typing import Tuple, Union
 
 from utils.img_utils import img_to_df, get_img_size
 from utils.plot_utils import plot_predictions
@@ -19,3 +21,21 @@ def predict_and_plot(image_path: str, output_filename: str, svm: SVC):
     predictions = svm.predict(sample_df)
 
     plot_predictions(predictions, sample_size, output_filename)
+
+def l(t: Union[np.ndarray, float]):
+    if t >= 1:
+        return 0
+    return 1 - t
+
+def calculate_cost(x: np.ndarray, y: np.ndarray, w: np.ndarray, b: float, c: float) -> float:
+    t = np.dot(y, (np.dot(x, w) + b))
+    return 0.5 * np.dot(w, w) + c * np.sum(l(t))
+
+def compute_gradient(x: np.ndarray, y: np.ndarray, w: np.ndarray, b: float, c: float) -> Tuple[np.ndarray, float]:
+    t = np.dot(y, (np.dot(x, w) + b))
+    w_gradient = w
+    b_gradient = 0
+    if t < 1:
+        w_gradient = w + c * np.sum(np.dot(y, x) * (-1))
+        b_gradient = c * np.sum(y) * (-1)
+    return w_gradient, b_gradient

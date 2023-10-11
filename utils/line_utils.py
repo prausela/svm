8b7c45f2b8f55298ddc74cdffc8fea2aff794e35
Paula Oseroff <oseroffp@gmail.com>
2023-10-11 14:23:48 -0300

    fix(line_utils): remove negative margins

--- utils/line_utils.py
+++ utils/line_utils.py
@@ -127,7 +127,7 @@ def __least_dist2line_points_idxs_by_class_for_all__(points: np.ndarray, line: L
 
 def least_dist2line_points_idxs_by_class(points: np.ndarray, line: Line, point_count: int = None,
                                          positive_points: int = None, negative_points: int = None
-                                         ) -> tuple[np.ndarray, np.ndarray]:
+                                         ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     
     point_count_is_none = point_count is None
     positive_and_negative_points_are_none = positive_points is None and negative_points is None
@@ -161,78 +161,113 @@ def least_dist2line_points_idxs_by_class(points: np.ndarray, line: Line, point_c
     
     return __least_dist2line_points_idxs_by_class_for_each__(points, line, positive_points, negative_points)
 
-def points_distances_to_line(line: Line, points: np.ndarray) -> np.ndarray:
+def points_distances_to_line(line: Line, points: np.ndarray, class_sign: np.ndarray) -> np.ndarray:
     distances = np.apply_along_axis(line.distance_to, 1, points)
-    abs_dists = abs(distances)
-    return abs_dists
+    distances = distances * class_sign
+    return distances
 
 X_POS = 0
 Y_POS = 1
 
-def support_vectors_line(dirPointA: np.ndarray, dirPointB: np.ndarray, traslPoint: np.ndarray) -> Line:
+def support_vectors_line(dirPointA: np.ndarray, dirPointB: np.ndarray, traslPoint: np.ndarray) -> tuple[Line, Line]:
     x1, x2, x_t = dirPointA[X_POS], dirPointB[X_POS], traslPoint[X_POS]
     y1, y2, y_t = dirPointA[Y_POS], dirPointB[Y_POS], traslPoint[Y_POS]
 
-    m = (x2 - x1) / (y2 - y1)
+    m =  (y2 - y1) / (x2 - x1)
 
     b_dir   = y2 - m * x2
     b_trasl = y_t - m * x_t
-    b = (b_dir + b_trasl) / 2
 
-    w = np.array([[m, -1, b]])
+    min_b = min(b_dir, b_trasl)
+    max_b = max(b_dir, b_trasl)
 
-    line = Line(w)
-    return line
+    b = min_b + (max_b - min_b) / 2
+    
+    wA = np.array([[m, -1, b]])
+    wB = np.array([[-m, 1, -b]])
 
-def __calculate_class_margin__(line: Line, class_points: np.ndarray) -> float:
-    class_dists = points_distances_to_line(line, class_points)
+    lineA = Line(wA)
+    lineB = Line(wB)
+    return lineA, lineB
+
+
+def __calculate_class_margin__(line: Line, class_points: np.ndarray, class_pred: np.ndarray) -> float:
+    class_dists = points_distances_to_line(line, class_points, class_pred)
     class_least_dist_idx = least_dist_idxs(class_dists, 1).item()
     class_margin = class_dists[class_least_dist_idx]
     return class_margin
-
-def __margin2line_from_points__(line: Line, classA_points: np.ndarray, classB_points: np.ndarray) -> float:
-    classA_margin = __calculate_class_margin__(line, classA_points)
-    classB_margin = __calculate_class_margin__(line, classB_points)
+    
+def __margin2line_from_points__(line: Line, classA_points: np.ndarray, classB_points: np.ndarray,
+                                classA_pred: np.ndarray, classB_pred: np.ndarray) -> float:
+    classA_margin = __calculate_class_margin__(line, classA_points, classA_pred)
+    if classA_margin < 0:
+        classA_margin = float("-inf")
+    classB_margin = __calculate_class_margin__(line, classB_points, classB_pred)
+    if classB_margin < 0:
+        classB_margin = float("-inf")
     margin = classA_margin + classB_margin
     return margin
 
-def max_margin_line_from_points(dir_points: np.ndarray, trasl_points: np.ndarray) -> tuple[float, Line]:
+def max_margin_line_from_points(dir_points: np.ndarray, trasl_points: np.ndarray,
+                                dir_pred: np.ndarray, trasl_pred: np.ndarray
+                                ) -> tuple[float, Line, tuple[np.ndarray, np.ndarray]]:
     dir_points_count = dir_points.shape[0]
     trasl_points_count = trasl_points.shape[0]
     max_margin = None
     max_margin_line = None
+    chosen_points_idxs = None
     for dir_i in range(dir_points_count):
         for dir_j in range(dir_i+1, dir_points_count):
             for trasl_idx in range(trasl_points_count):
-                line = support_vectors_line(dir_points[dir_i], dir_points[dir_j], trasl_points[trasl_idx])
+                curr_points_idxs = (np.array([dir_i, dir_j]), np.array([trasl_idx]))
+                lineA, lineB = support_vectors_line(dir_points[dir_i], dir_points[dir_j], trasl_points[trasl_idx])
 
-                margin = __margin2line_from_points__(line, dir_points, trasl_points)
+                margin = __margin2line_from_points__(lineA, dir_points, trasl_points, dir_pred, trasl_pred)
                 if max_margin is None or margin > max_margin:
                     max_margin = margin
-                    max_margin_line = line
+                    max_margin_line = lineA
+                    chosen_points_idxs = curr_points_idxs
+                
+                margin = __margin2line_from_points__(lineB, dir_points, trasl_points, dir_pred, trasl_pred)
+                if max_margin is None or margin > max_margin:
+                    max_margin = margin
+                    max_margin_line = lineB
+                    chosen_points_idxs = curr_points_idxs
 
-    return max_margin, max_margin_line
+    return max_margin, max_margin_line, chosen_points_idxs
 
-def __max_margin_line_by_class_points__(positive_points: np.ndarray, negative_points: np.ndarray) -> tuple[float, Line]:
-    dir_by_pos_margin, dir_by_pos_line = max_margin_line_from_points(positive_points, negative_points)
-    dir_by_neg_margin, dir_by_neg_line = max_margin_line_from_points(negative_points, positive_points)
+def __max_margin_line_by_class_points__(positive_points: np.ndarray, negative_points: np.ndarray,
+                                        pos_pred: np.ndarray, neg_pred: np.ndarray) -> tuple[float, Line,
+                                                                                             tuple[np.ndarray, 
+                                                                                                   np.ndarray]]:
+    dir_by_pos_margin, dir_by_pos_line, dir_by_pos_chosen_idxs = max_margin_line_from_points(positive_points, negative_points, pos_pred, neg_pred)
+    dir_by_neg_margin, dir_by_neg_line, dir_by_neg_chosen_idxs = max_margin_line_from_points(negative_points, positive_points, neg_pred, pos_pred)
 
     if dir_by_neg_margin is None or (dir_by_pos_margin is not None and dir_by_pos_margin >= dir_by_neg_margin):
-        return dir_by_pos_margin, dir_by_pos_line
+        return dir_by_pos_margin, dir_by_pos_line, dir_by_pos_chosen_idxs
 
-    return dir_by_neg_margin, dir_by_neg_line
+    return dir_by_neg_margin, dir_by_neg_line, (dir_by_neg_chosen_idxs[1], dir_by_neg_chosen_idxs[0])
 
 def maximize_step_perceptron_line_margin(df: pd.DataFrame, out_col: str, perceptron: Perceptron, 
                                          points2decide : int = None, positive_points2decide: int = None, 
                                          negative_points2decide: int = None, only_correct : bool = True
-                                         ) -> tuple[Perceptron, float]:
+                                         ) :#-> tuple[Perceptron, float, tuple[np.ndarray, np.ndarray], tuple[np.ndarray, np.ndarray]]:
+    
     points, _ = sample2points(df, out_col, perceptron, only_correct)
     line = step_perceptron_line(perceptron)
-    pos_idxs, neg_idxs = least_dist2line_points_idxs_by_class(points, 
-                                                              line, points2decide, 
+    pos_idxs, neg_idxs = least_dist2line_points_idxs_by_class(points, line, points2decide, 
                                                               positive_points2decide, 
                                                               negative_points2decide)
-    margin, line = __max_margin_line_by_class_points__(points[pos_idxs], points[neg_idxs])
+    sel_points_pos = points[pos_idxs]
+    sel_points_neg = points[neg_idxs]
+    pos_pred = np.apply_along_axis(lambda x : perceptron.predict(x=x), 1, sel_points_pos)
+    neg_pred = np.apply_along_axis(lambda x : perceptron.predict(x=x), 1, sel_points_neg)
+    margin, line, chosen_points_class_idxs = __max_margin_line_by_class_points__(sel_points_pos, sel_points_neg, pos_pred, neg_pred)
     max_margin_perceptron = Perceptron(line.coefficients(), step_activation)
-    return max_margin_perceptron, margin
+    chosen_points_pos_class_idxs = chosen_points_class_idxs[0]
+    chosen_points_neg_class_idxs = chosen_points_class_idxs[1]
+    chosen_points_pos = points[pos_idxs[chosen_points_pos_class_idxs]]
+    chosen_points_neg = points[neg_idxs[chosen_points_neg_class_idxs]]
+    
+    return max_margin_perceptron, margin, points, (chosen_points_pos, chosen_points_neg), (sel_points_pos, sel_points_neg)
 

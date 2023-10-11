28fa4bf481293b54f813396751bea43432f309db
Paula Domingues <pdomingues@itba.edu.ar>
2023-10-11 03:39:50 -0300

    feat: best kernel and C search updated

--- utils/svm_best_kernel_c.py
+++ utils/svm_best_kernel_c.py
@@ -1,67 +1,81 @@
+import os
+
 import numpy as np
 import pandas as pd
-
-from utils.confusion_matrix import calculate_relative_confusion_matrix, \
-    calculate_per_label_confusion_matrix_from_confusion_matrix, metrics
-from utils.data_split import k_fold_split
-from utils.plotter import plot_confusion_matrix
-from utils.svm_utils import get_svm_by_c_kernel
-
-
-def print_results(conf_matrix, per_label_conf_matrix):
-    print("Confusion Matrix:")
-    print(conf_matrix)
-
-    # print("Per Label Confusion Matrix:")
-    # print(per_label_conf_matrix)
-
-    metrics_result = metrics(per_label_conf_matrix)
-
-    precision_values = [values['Precision'] for values in metrics_result.values()]
-    accuracy_values = [values['Accuracy'] for values in metrics_result.values()]
-
-    average_precision = sum(precision_values) / len(metrics_result)
-    average_accuracy = sum(accuracy_values) / len(metrics_result)
-
-    precision_std = np.std(precision_values)
-    accuracy_std = np.std(accuracy_values)
-
-    print(f'Average Precision: {average_precision}')
-    print(f'Average Accuracy: {average_accuracy}')
-    print(f'Standard Deviation of Precision: {precision_std}')
-    print(f'Standard Deviation of Accuracy: {accuracy_std}')
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
 
 
 def get_svm_confusion_matrix(cow_df: pd.DataFrame, grass_df: pd.DataFrame, sky_df: pd.DataFrame):
-    iters = 1
-    kernels = ['linear', 'sigmoid', 'rbf', 'poly']
-    c_values = np.arange(0.2, 2.2, 0.2)  # Start from 0.2, end at 2.0, with a step of 0.2
-    df = pd.concat([cow_df, grass_df, sky_df], ignore_index=True)
+    output_dir = 'output/graphs/'
+    os.makedirs(output_dir, exist_ok=True)
 
+    iters = 10
+    kernels = ['linear', 'sigmoid', 'rbf', 'poly']
+    c_values = np.arange(0.5, 20.5, 0.5)  # Start from 0.5, end at 20.0, with a step of 0.5
+
+    df = pd.concat([cow_df, grass_df, sky_df], ignore_index=True)
     dataframes_list = []
+
     for _ in range(iters):
-        train_df, test_df = k_fold_split(df, k=5)
-        x = train_df[["R", "G", "B"]].to_numpy()
-        y = train_df["Class"].to_numpy()
-        dataframes_list.append((train_df, test_df, x, y))
+        train_df, test_df = train_test_split(df, test_size=0.2)  # Adjust test_size as needed
+        x_train = train_df[["R", "G", "B"]].to_numpy()
+        y_train = train_df["Class"].to_numpy()
+        dataframes_list.append((train_df, test_df, x_train, y_train))
 
     class_labels = np.array(['cow', 'grass', 'sky'])
+
     for kernel in kernels:
         for c in c_values:
-            print(f"{kernel} {c}")
-            predictions = []
             to_predict = []
+            predictions = []
+
             for index in range(iters):
-                train_df, test_df, x, y = dataframes_list[index]
-                svm = get_svm_by_c_kernel(x, y, c, kernel)
-                print(f"{kernel} {c} svm generated")
-                predictions.append(svm.predict(test_df[["R", "G", "B"]]))
-                to_predict.append(test_df["Class"])
-                print(f"{kernel} {c} values predicted")
-            result_df = pd.DataFrame({'predictions': predictions, 'to_predict': to_predict})
-            print(result_df)
-            conf_mat = calculate_relative_confusion_matrix(class_labels, result_df['predictions'],
-                                                           result_df['to_predict'])
-            per_label_conf_matrix = calculate_per_label_confusion_matrix_from_confusion_matrix(conf_mat)
-            print_results(conf_mat, per_label_conf_matrix)
-            plot_confusion_matrix(conf_mat, "Matriz de confusión", f"./output/graphics/{kernel}{c}_conf_mat.png", ".2f")
+                train_df, test_df, x_train, y_train = dataframes_list[index]
+                x_test = test_df[["R", "G", "B"]].to_numpy()
+                y_test = test_df["Class"].to_numpy()
+
+                svm = SVC(C=c, kernel=kernel)
+                svm.fit(x_train, y_train)
+
+                iteration_predictions = svm.predict(x_test)
+                to_predict.extend(y_test)
+                predictions.extend(iteration_predictions)
+
+            cm = confusion_matrix(to_predict, predictions, labels=class_labels, normalize='true')
+
+            plt.figure(figsize=(8, 6))
+            plt.imshow(cm, interpolation='nearest', cmap=sns.cubehelix_palette(as_cmap=True, rot=.2, gamma=.5))
+            plt.title(f"Normalized Confusion Matrix for {kernel} kernel, c = {c:.1f}")
+            plt.colorbar()
+
+            tick_marks = np.arange(len(class_labels))
+            plt.xticks(tick_marks, class_labels, rotation=45)
+            plt.yticks(tick_marks, class_labels)
+
+            fmt = '.2f'
+            thresh = cm.max() / 2.
+            for i in range(len(class_labels)):
+                for j in range(len(class_labels)):
+                    plt.text(j, i, format(cm[i, j], fmt),
+                             horizontalalignment="center",
+                             color="white" if cm[i, j] > thresh else "black")
+
+            plt.tight_layout()
+            os.makedirs(f"output/graphs/{kernel}", exist_ok=True)
+            plt.savefig(f"output/graphs/{kernel}/confusion_matrix_{c:.1f}.png")
+            plt.close()
+
+            # Calculate precision and accuracy
+            precision = precision_score(to_predict, predictions, average='weighted')
+            accuracy = accuracy_score(to_predict, predictions)
+            error_precision = 1 - precision
+            error_accuracy = 1 - accuracy
+
+            print(f"{kernel} kernel, c = {c:.1f}")
+            print(f"Precision: {precision:.2f}, Error (Precision): {error_precision:.4f}")
+            print(f"Accuracy: {accuracy:.2f}, Error (Accuracy): {error_accuracy:.4f}")
